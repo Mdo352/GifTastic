e1697504633fc63e113a38a0c9455c7b6f764125
Mike <mjeanjacques90@gmail.com>
2019-05-03 17:44:49 -0400

    set limit to 10 gifs  and added the rating

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,19 +3,33 @@ var cartoons = ['Spongebob', 'Futurama', 'South Park'];
 function displayGif() {
 
     var show = $(this).attr("data-name");
-    console.log(show);
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=2cbOR934j9f4PwJLNJwwHhYIE2XIDONd&tag="+show;
+    // console.log(show);
+    // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=2cbOR934j9f4PwJLNJwwHhYIE2XIDONd&tag="+show+"&limit=5";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=2cbOR934j9f4PwJLNJwwHhYIE2XIDONd&limit=10";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+        var results = response.data;
+        console.log(response);
 
-        var imageUrl = response.data.image_original_url;
-        var showImage = $("<img>");
-        showImage.attr("src", imageUrl);
-        showImage.attr("alt", "Simpsons Image");
-        $("#gifTarget").prepend(showImage);
+        for (var i = 0; i < response.data.length; i++){
+            var gifDiv = $("<div>");
+            var rating = $("<p>").text("Rating: " + results[i].rating); 
+
+            var showGif = $("<img>");
+            showGif.attr("src", results[i].images.fixed_height.url);
+            // var imageUrl = results[i].image_original_url;
+            // showGif.attr("src", imageUrl);
+            showGif.attr("alt", "Gif Image of a cartoon");
+
+            gifDiv.prepend(rating);
+            gifDiv.prepend(showGif);
+
+
+            $("#gifTarget").prepend(gifDiv);
+        }
 
     });
 
