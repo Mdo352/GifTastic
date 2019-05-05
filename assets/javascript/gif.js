919489902ed51b621b7b1c30068fba1cea8109c5
Mike <mjeanjacques90@gmail.com>
2019-05-04 22:13:54 -0400

    delete comments

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -5,8 +5,6 @@ var isGifClicked = "still";
 function displayGif() {
 
     var show = $(this).attr("data-name");
-    // console.log(show);
-    // var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=2cbOR934j9f4PwJLNJwwHhYIE2XIDONd&tag="+show+"&limit=5";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=2cbOR934j9f4PwJLNJwwHhYIE2XIDONd&limit=10";
 
     $.ajax({
@@ -14,7 +12,6 @@ function displayGif() {
       method: "GET"
     }).then(function(response) {
         var results = response.data;
-        console.log(response);
         
         for (var i = 0; i < response.data.length; i++){
             var gifDiv = $("<div>");
@@ -44,8 +41,6 @@ function displayGif() {
 
 function changeSrc(){
     isGifClicked = true;
-    console.log('clicked');
-    console.log(isGifClicked);
     stillGif.attr("src", results[i].images.fixed_height.url);
 }
 
@@ -64,13 +59,8 @@ function buttons(){
     
 }
 $(document).on("click",".gifImg", function(){
-    // alert("clicked")
-    // debugger;
     var state = $(this).attr("data-state");
-    console.log($(this).attr("data-state"));
-    // if(!state){
-    //     return;
-    // }
+    // console.log($(this).attr("data-state"));
     if (state === "still"){
         $(this).attr("src", $(this).attr("data-motion"));
         $(this).attr("data-state", "motion");
